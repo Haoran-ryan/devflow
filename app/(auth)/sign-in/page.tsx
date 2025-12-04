c077aa2df3d24ea8a5aaad6d1708f6e74ff292d9
Ryan Guo <ryanguo@MacBookPro.tpg_fwa>
2025-12-04 21:38:27 +1100

    email auth setup

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 import React from "react";
+import { success } from "zod";
 
 const page = () => {
-  return <div>sign-in page coming soon</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })} // placeholder handler
+    />
+  );
 };
 
 export default page;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 import React from "react";
 
 const page = () => {
-  return <div>sign-up page coming soon</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })} // placeholder handler/>;
+    />
+  );
 };
 
 export default page;
